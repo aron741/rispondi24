bb41d717db2ab92992e84587a3f6dcb34c119284
aron741 <aron.1100022@gmail.com>
2026-04-08 16:51:43 +0200

    update google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,14 +19,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <meta
+          name="google-site-verification"
+          content="0y7OLVtGsKWtlr0s33VKfaPZaleQnyidx0qHyhbznlw"
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
